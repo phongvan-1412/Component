43b945f104943d86a76d328631f251133d8419bb
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2024-04-26 01:49:18 +0700

    add main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+class Label {
+  constructor({ ID, title, srcIcon, titleColor}) {
+    this._divWrapper = document.getElementById(ID);
+
+    this._title = title;
+    if (this._divWrapper.getAttribute("data-title") !== "") {
+      this._title = this._divWrapper.getAttribute("data-title");  
+    }
+    
+    this._srcIcon = srcIcon;
+    if (this._divWrapper.getAttribute("data-icon") !== "") {
+      this._srcIcon = "/images/" + this._divWrapper.getAttribute("data-icon");  
+    }
+
+    this._titleColor = titleColor;
+    if (this._divWrapper.getAttribute("data-title-color") !== "") {
+      this._titleColor = this._divWrapper.getAttribute("data-title-color");  
+    }
+   
+    this._template = `
+      <span class="title ${this._titleColor}">${this._title}</span>
+      <img class="icon" src="${this._srcIcon}"/>
+    `;
+  }
+
+  init() {
+    this._divWrapper.innerHTML = this._template;
+  }
+}
+
+window.onload = function() {
+  for (let index = 1; index < 10; index++) {
+    new Label({
+      ID: `labelIcon${index}`,
+      title: "TITLE",
+      srcIcon: "/images/add.png",
+      titleColor: "black"
+    }).init();
+  }
+};
